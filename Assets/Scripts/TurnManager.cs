d1ac65d662af14f7be14126ee88e5f4fb0ebf987
STD\s20227005 <titiko225@icloud.com>
2025-01-16 17:56:39 +0900

    捕虜駒の反乱イベント作成

---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,32 @@
+// --------------------------------------------------------- 
+// TurnManager.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+using UnityEngine;
+using System.Collections;
+public class TurnManager : MonoBehaviour
+{
+ #region variable 
+ #endregion
+ #region property
+ #endregion
+ #region method
+ 
+ private void Awake()
+ {
+
+ }
+ 
+ private void Start ()
+ {
+
+ }
+
+ private void Update ()
+ {
+
+ }
+ #endregion
+}
