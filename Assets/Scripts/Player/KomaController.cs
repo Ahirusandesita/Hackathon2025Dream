c8b7f86565cd0c3fa8c396a3b2c4cd901b3c2fac
STD\s20227005 <titiko225@icloud.com>
2025-01-16 19:13:03 +0900

    no message

--- Assets/Scripts/Player/KomaController.cs
+++ Assets/Scripts/Player/KomaController.cs
@@ -8,12 +8,38 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
-public class KomaController : MonoBehaviour
+public class KomaController : MonoBehaviour, IInjectPlayer
 {
     [SerializeField]
     private Ban _ban = default;
     private List<Koma> _ownKomas = default;
+    private PlayerNumber playerNumber;
+
+    private void Initialize()
+    {
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        //反乱したとき　演出とかするときは演出開始処理をforeachの中に書く
+        gameManager.Opponent(playerNumber).GetComponent<POWManager>().OnRebellion += (eventData, sender) =>
+        {
+            foreach (Koma koma in eventData.Rebellions)
+            {
+                _ownKomas.Add(koma);
+            }
+        };
+
+        //反乱されたとき　演出とかするときはeventData.Rebillionsを使って演出処理をする
+        gameManager.Me(playerNumber).GetComponent<POWManager>().OnRebellion += (eventData, sender) =>
+        {
+            _ownKomas.RemoveAll(item => eventData.Rebellions.Contains(item));
+        };
+    }
+    public void InjectPlayer(PlayerNumber playerNumber)
+    {
+        this.playerNumber = playerNumber;
+        Initialize();
+    }
 
     public void OnClickKoma(Vector2Int position)
     {
