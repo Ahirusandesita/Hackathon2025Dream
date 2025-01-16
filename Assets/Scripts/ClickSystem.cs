fe23883d23edf65fadc8c84abeeb3a0566c7f759
STD\s20227053 <20227053-morishitayusei@hcs.ac.jp>
2025-01-16 19:02:15 +0900

    マウスシステム追加

---
+++ Assets/Scripts/ClickSystem.cs
@@ -0,0 +1,61 @@
+// --------------------------------------------------------- 
+// ClickSystem.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+using UnityEngine;
+using System.Collections;
+public class ClickSystem : MonoBehaviour
+{
+
+    #region variable 
+
+    private float _rayRenge = 30f;
+
+    private Vector3 _rayHitPotision;
+
+    private Masu _hitPositionMasu;
+
+    #endregion
+    #region property
+    #endregion
+    #region method
+
+    private void Awake()
+ {
+
+ }
+ 
+ private void Start ()
+ {
+
+ }
+
+ private void Update ()
+ {
+        // 左クリックしたとき
+        if (Input.GetMouseButton(0))
+        {
+            Ray ClickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            Debug.DrawRay(ClickRay.origin, ClickRay.direction * _rayRenge, Color.green);
+
+            // レイキャスト発射
+            if (Physics.Raycast(ClickRay, out hit, _rayRenge))
+            {
+                _rayHitPotision = hit.point;
+
+                _hitPositionMasu = hit.collider.gameObject.GetComponent<Masu>();
+
+                //Debug.Log("当たった所　" + _rayHitPotision);   
+            }
+            else
+            {
+                return;
+            }
+        }
+
+ #endregion
+}
