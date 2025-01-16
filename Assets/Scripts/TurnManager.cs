f61bae4a596fc85eab01e30c41ff76c6b89e1edd
STD\s20227005 <titiko225@icloud.com>
2025-01-16 19:32:37 +0900

    現ターンのプレイヤーナンバーのゲッター追加

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -11,8 +11,19 @@ using System;
 public class TurnManager : MonoBehaviour
 {
     public event Action<PlayerNumber> OnTurnEnd;
+    private PlayerNumber nowTurnPlayerNumber = PlayerNumber.Player1;
+    public PlayerNumber NowTurnPlayerNumber => nowTurnPlayerNumber;
     public void TurnEnd(PlayerNumber playerNumber)
     {
         OnTurnEnd?.Invoke(playerNumber);
+        switch (playerNumber)
+        {
+            case PlayerNumber.Player1:
+                nowTurnPlayerNumber = PlayerNumber.Player2;
+                break;
+            case PlayerNumber.Player2:
+                nowTurnPlayerNumber = PlayerNumber.Player1;
+                break;
+        }
     }
 }
