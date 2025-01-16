094839829202fb9f767eb44e78a3cccd43190b5b
STD\s20227082 <20227082-takayanagisora@gmail.com>
2025-01-16 16:50:24 +0900

    追加 >> 駒クラス

---
+++ Assets/Scripts/Koma/Koma.cs
@@ -0,0 +1,14 @@
+// --------------------------------------------------------- 
+// Koma.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+
+using UnityEngine;
+using System.Collections;
+
+public abstract class Koma : MonoBehaviour
+{
+
+}
