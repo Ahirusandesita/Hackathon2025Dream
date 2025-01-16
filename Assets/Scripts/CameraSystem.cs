a3fc14315c79a6c5ce1adf96d4ff64f1de43eb2f
STD\s20227082 <20227082-takayanagisora@gmail.com>
2025-01-16 19:17:42 +0900

    追加 >> DOTween, MainScene, CameraSystem

---
+++ Assets/Scripts/CameraSystem.cs
@@ -0,0 +1,20 @@
+// --------------------------------------------------------- 
+// CameraSystem.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+
+using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class CameraSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _player1Camera = default;
+    [SerializeField]
+    private Transform _player2Camera = default;
+
+
+}
