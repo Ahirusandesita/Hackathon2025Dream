4f8d7f9ae231b05443298b8b605f8fc0cb8248cc
STD\s20227005 <titiko225@icloud.com>
2025-01-16 16:59:28 +0900

    各プレイヤー毎のマネージャーにアクセスするためのManager作成

---
+++ Assets/Scripts/PrisonerOfWar/POWManager.cs
@@ -0,0 +1,32 @@
+// --------------------------------------------------------- 
+// POWManager.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+using UnityEngine;
+using System.Collections;
+public class POWManager : MonoBehaviour
+{
+ #region variable 
+ #endregion
+ #region property
+ #endregion
+ #region method
+ 
+ private void Awake()
+ {
+
+ }
+ 
+ private void Start ()
+ {
+
+ }
+
+ private void Update ()
+ {
+
+ }
+ #endregion
+}
---
+++ Assets/Scripts/PrisonerOfWar/POWPiece.cs
@@ -0,0 +1,32 @@
+// --------------------------------------------------------- 
+// POWPiece.cs 
+// 
+// CreateDay: 
+// Creator  : 
+// --------------------------------------------------------- 
+using UnityEngine;
+using System.Collections;
+public class POWPiece : MonoBehaviour
+{
+ #region variable 
+ #endregion
+ #region property
+ #endregion
+ #region method
+ 
+ private void Awake()
+ {
+
+ }
+ 
+ private void Start ()
+ {
+
+ }
+
+ private void Update ()
+ {
+
+ }
+ #endregion
+}
