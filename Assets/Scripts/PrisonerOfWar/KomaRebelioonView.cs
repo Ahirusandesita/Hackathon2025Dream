fc44c65d355fd06aab8ddd8aba3f0e77c7511f88
STD\s20227005 <titiko225@icloud.com>
2025-01-17 16:55:59 +0900

    canvas微調整

--- Assets/Scripts/PrisonerOfWar/KomaRebelioonView.cs
+++ Assets/Scripts/PrisonerOfWar/KomaRebelioonView.cs
@@ -36,16 +36,16 @@ public class KomaRebelioonView : MonoBehaviour, IInject<GameManager>, IInject<IR
             for (int k = 0; k < POWGroupAssets[i].KomaAssets.Count; k++)
             {
                 Vector2 position = firstPosition.GetComponent<RectTransform>().position;
-                position.x += (komaFrame.GetComponent<RectTransform>().lossyScale.x * 120f) * k;
-                position.y -= (komaFrame.GetComponent<RectTransform>().lossyScale.y * 120f) * i;
+                position.x += (komaFrame.GetComponent<RectTransform>().lossyScale.x * 60f) * k;
+                position.y -= (komaFrame.GetComponent<RectTransform>().lossyScale.y * 60f) * i;
                 Image image = Instantiate(komaFrame, position, Quaternion.identity, this.transform);
 
                 image.sprite = POWGroupAssets[i].KomaAssets[k].Icon;
             }
 
             Vector2 textPosition = firstPosition.GetComponent<RectTransform>().position;
-            textPosition.x += (komaFrame.GetComponent<RectTransform>().lossyScale.x * 120f) * POWGroupAssets[i].KomaAssets.Count;
-            textPosition.y -= (komaFrame.GetComponent<RectTransform>().lossyScale.y * 120f) * i;
+            textPosition.x += 15f;
+            textPosition.y -= (komaFrame.GetComponent<RectTransform>().lossyScale.y * 60f) * i;
             TextMeshProUGUI textMeshProUGUI = Instantiate(rebellionValueText, textPosition, Quaternion.identity, this.transform);
             textMeshProUGUI.text = POWGroupAssets[i].NumberOfPOWNeededForRebellion.ToString();
         }
