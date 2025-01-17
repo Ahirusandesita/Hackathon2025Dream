aac0b6a479604b8dfe0279fc93743e9252cbca73
STD\s20227082 <20227082-takayanagisora@gmail.com>
2025-01-17 19:51:08 +0900

    追加 >> 攻撃

--- Assets/Scripts/Player/KomaController.cs
+++ Assets/Scripts/Player/KomaController.cs
@@ -21,17 +21,18 @@ public class KomaController : MonoBehaviour, IInjectPlayer
     private List<Koma> _ownKomas = default;
     private PlayerNumber _myPlayerNumber = default;
     private PhaseManager _phaseManager = default;
+    private GameManager _gameManager = default;
 
     public List<Koma> OwnKomas { get => _ownKomas; set => _ownKomas = value; }
 
     private void Initialize()
     {
-        GameManager gameManager = FindObjectOfType<GameManager>();
+        _gameManager = FindObjectOfType<GameManager>();
         // 対局開始イベントを購読し、初期駒を配置
-        gameManager.OnWaitGameStart += SetInitializeKomas;
+        _gameManager.OnWaitGameStart += SetInitializeKomas;
 
         //反乱したとき　演出とかするときは演出開始処理をforeachの中に書く
-        gameManager.Opponent(_myPlayerNumber).GetComponent<POWManager>().OnRebellion += (eventData, sender) =>
+        _gameManager.Opponent(_myPlayerNumber).GetComponent<POWManager>().OnRebellion += (eventData, sender) =>
         {
             foreach (Koma koma in eventData.Rebellions)
             {
@@ -40,7 +41,7 @@ public class KomaController : MonoBehaviour, IInjectPlayer
         };
 
         //反乱されたとき　演出とかするときはeventData.Rebillionsを使って演出処理をする
-        gameManager.Me(_myPlayerNumber).GetComponent<POWManager>().OnRebellion += (eventData, sender) =>
+        _gameManager.Me(_myPlayerNumber).GetComponent<POWManager>().OnRebellion += (eventData, sender) =>
         {
             _ownKomas.RemoveAll(item => eventData.Rebellions.Contains(item));
         };
@@ -81,6 +82,57 @@ public class KomaController : MonoBehaviour, IInjectPlayer
 
     }
 
+    public bool IsExistOwnKomaAtPosition(Vector2Int position)
+    {
+        foreach (var koma in _ownKomas)
+        {
+            if (position == koma.CurrentPosition)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public bool IsExistOwnKomaAtPosition(Vector2Int position, out Vector2Int[] movablePositions)
+    {
+        foreach (var koma in _ownKomas)
+        {
+            if (position == koma.CurrentPosition)
+            {
+                movablePositions = koma.KomaAsset.MovableDirection;
+                return true;
+            }
+        }
+
+        movablePositions = null;
+        return false;
+    }
+
+    public void TakeAttack(Vector2Int takeAttackPosition)
+    {
+        int i;
+        for (i = 0; i < _ownKomas.Count; i++)
+        {
+            if (_ownKomas[i].CurrentPosition == takeAttackPosition)
+            {
+                break;
+            }
+        }
+
+        // もし取られた駒が捕虜だったら、捕虜にしない
+        if (_ownKomas[i].MyAbsolutePlayerNumber != _myPlayerNumber)
+        {
+            _gameManager.Me(_myPlayerNumber).GetComponent<POWManager>().CancelPOW(_ownKomas[i]);
+            _gameManager.Opponent(_myPlayerNumber).GetComponent<POWManager>().TurnedIntoPOW(_ownKomas[i]);
+        }
+        // 相手に駒を渡す
+        _gameManager.Opponent(_myPlayerNumber).GetComponent<KomaController>().OwnKomas.Add(_ownKomas[i]);
+        _ownKomas.RemoveAt(i);
+        _ban.RemoveKoma(takeAttackPosition);
+    }
+
     private async UniTask SetInitializeKomas()
     {
         _ownKomas = new List<Koma>();
@@ -90,8 +142,8 @@ public class KomaController : MonoBehaviour, IInjectPlayer
             Vector2Int masuPosition = _initialKomaPositionAsset.InitialPositions[i].Position;
             Vector3 worldPosition = _banUI.GetWorldPosition(masuPosition);
             Koma koma = Instantiate(
-                _initialKomaPositionAsset.InitialPositions[i].Koma, 
-                worldPosition, 
+                _initialKomaPositionAsset.InitialPositions[i].Koma,
+                worldPosition,
                 Quaternion.identity
                 );
             _ownKomas.Add(koma);
--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -7,11 +7,21 @@
 
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
 
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField]
     private PlayerNumber _playerNumber = default;
+    [SerializeField]
+    private KomaController _komaController = default;
+    private Ban _ban = default;
+    private ClickSystem _clickSystem = default;
+    private GameManager _gameManager = default;
+    private Vector2Int[] _movableWorldPositions = default;
+
     public PlayerNumber PlayerNomber => _playerNumber;
 
     public static int GetMoveDirectionCoefficient(PlayerNumber playerNumber)
@@ -24,6 +34,61 @@ public class PlayerManager : MonoBehaviour
                 return 1;
             default:
                 throw new System.ArgumentOutOfRangeException();
+        }
+    }
+
+    private void Awake()
+    {
+        _gameManager = transform.root.GetComponent<GameManager>();
+        _ban = FindObjectOfType<Ban>();
+        _clickSystem = FindObjectOfType<ClickSystem>();
+
+        PhaseManager phaseManager = transform.root.GetComponent<PhaseManager>();
+        phaseManager.OnAttackStart += playerNumber =>
+        {
+            List<Vector2Int> ownKomaPositions = new List<Vector2Int>();
+            foreach (var koma in _komaController.OwnKomas)
+            {
+                ownKomaPositions.Add(koma.CurrentPosition);
+            }
+
+            _clickSystem.OnClickMasu += OnClickMasuAtSelectOwn;
+            BanUI.Get().Blink(ownKomaPositions.ToArray());
+        };
+
+        phaseManager.OnAttackEnd += playerNumber =>
+        {
+
+        };
+
+    }
+
+    /// <summary>
+    /// ©•ª‚Ì‹î‚ğ‘I‘ğ‚·‚é‚Æ‚«
+    /// </summary>
+    /// <param name="masu"></param>
+    private void OnClickMasuAtSelectOwn(Masu masu)
+    {
+        // ©•ª‚Ì‹î‚ª‰Ÿ‚³‚ê‚½
+        if (_komaController.IsExistOwnKomaAtPosition(masu.OwnPosition, out Vector2Int[] movablePosition))
+        {
+            _movableWorldPositions = _ban.GetAttackablePosition(masu.OwnPosition, movablePosition);
+            _clickSystem.OnClickMasu -= OnClickMasuAtSelectOwn;
+            _clickSystem.OnClickMasu += OnClickMasuAtAttack;
+        }
+    }
+
+    /// <summary>
+    /// UŒ‚‰Â”\‚È‹î‚ğ‘I‘ğ‚·‚é‚Æ‚«
+    /// </summary>
+    /// <param name="masu"></param>
+    private void OnClickMasuAtAttack(Masu masu)
+    {
+        // ‘Šè‚Ì‹î‚ª‰Ÿ‚³‚ê‚½
+        if (_movableWorldPositions.Contains(masu.OwnPosition))
+        {
+            _gameManager.Opponent(_playerNumber).GetComponent<KomaController>().TakeAttack(masu.OwnPosition);
+            _clickSystem.OnClickMasu -= OnClickMasuAtAttack;
         }
     }
 }
