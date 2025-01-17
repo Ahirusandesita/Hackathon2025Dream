929b719170fd7b4e466fcbf7f0021f46de34ecd0
STD\s20227039 <20227039-satoshizuku@hcs.ac.jp>
2025-01-17 17:45:11 +0900

    BanBlinkのカラー設定

--- Assets/Scripts/Ban/BanBlinkObj.cs
+++ Assets/Scripts/Ban/BanBlinkObj.cs
@@ -11,22 +11,24 @@ using DG.Tweening;
 public class BanBlinkObj : MonoBehaviour
 {
     #region variable 
-    private const float BLINK_TIME = 0.5f;
-
+    private Transform _transform = default;
     private Renderer _render = default;
+    private Vector3 _scale = default;
     #endregion
 
     #region property
 
     #endregion
 
-    #region method
+    #region Unity method
     /// <summary>
     /// èâä˙âªèàóù
     /// </summary>
     private void Awake()
     {
+        _transform = transform;
         _render = GetComponent<Renderer>();
+        _scale = _transform.localScale;
         enabled = false;
     }
 
@@ -36,9 +38,8 @@ public class BanBlinkObj : MonoBehaviour
     private void OnEnable()
     {
         _render.enabled = true;
-        _render.material.color = Color.white;
+        _render.material.DOFade(0, 0.6f);
 
-        _render.material.DOFade(0, 0.5f).SetLoops(-1, LoopType.Yoyo);
     }
 
     /// <summary>
@@ -48,6 +49,18 @@ public class BanBlinkObj : MonoBehaviour
     {
         _render.material.DOKill();
         _render.enabled = false;
+    }
+    #endregion
+
+    #region Set method
+    /// <summary>
+    /// <para>SetColor</para>
+    /// <para>êFÇê›íËÇµÇ‹Ç∑</para>
+    /// </summary>
+    /// <param name="color"></param>
+    public void SetColor(Color color)
+    {
+        _render.material.color = color;
     }
     #endregion
 }
