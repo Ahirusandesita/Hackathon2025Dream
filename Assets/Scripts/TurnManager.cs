5484d6b9d60ea5709dacd5fcdc6e4b5df227649b
STD\s20227005 <titiko225@icloud.com>
2025-01-18 10:25:12 +0900

    no message

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -8,13 +8,15 @@ using UnityEngine;
 using System.Collections;
 using System;
 
-public class TurnManager : MonoBehaviour
+public class TurnManager : MonoBehaviour,IInject<GameManager>
 {
     /// <summary>
     /// PlayerNumber : ターン終わった人のプレイヤーのナンバー
     /// </summary>
     public event Action<PlayerNumber> OnTurnEnd;
     private PlayerNumber nowTurnPlayerNumber = PlayerNumber.Player1;
+    private GameManager gameManager;
+
     public PlayerNumber NowTurnPlayerNumber => nowTurnPlayerNumber;
     public void TurnEnd(PlayerNumber playerNumber)
     {
@@ -28,5 +30,19 @@ public class TurnManager : MonoBehaviour
                 nowTurnPlayerNumber = PlayerNumber.Player1;
                 break;
         }
+    }
+
+    void IInject<GameManager>.Inject(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+
+        //テスト
+
+        gameManager.GetComponent<PhaseManager>().OnRebellionCheckEnd += (playerNumber) =>
+        {
+            TurnEnd(playerNumber);
+
+            gameManager.GetComponent<IPhaseChanger>().AttackStart(nowTurnPlayerNumber);
+        };
     }
 }
