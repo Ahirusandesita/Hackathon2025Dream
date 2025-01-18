f1a3cbe34c957f1092e9fd10e741313ba7471a77
STD\s20227039 <20227039-satoshizuku@hcs.ac.jp>
2025-01-18 10:11:48 +0900

    Kingクラス中身削除

--- Assets/Scripts/Koma/King.cs
+++ Assets/Scripts/Koma/King.cs
@@ -5,46 +5,8 @@
 // Creator  : Shizuku
 // --------------------------------------------------------- 
 
-using UnityEngine;
-using System;
 
-[RequireComponent(typeof(Koma))]
-public class King : MonoBehaviour
+public class King : Koma
 {
-	[SerializeField]
-	private KomaController _komaCtrl = default;
-	private Koma _myKing = default;
 
-	public event Action OnDisappearKing;
-
-	/// <summary>
-	/// ‰Šú‰»ˆ—
-	/// </summary>
-    private void Awake()
-    {
-		_myKing = GetComponent<Koma>();
-
-		GameManager gManager = FindAnyObjectByType<GameManager>();
-		if(_myKing.MyPlayerNumber == PlayerNumber.Player1)
-        {
-			_komaCtrl = gManager.Player1.GetComponent<KomaController>();
-        }
-		else
-        {
-			_komaCtrl = gManager.Player2.GetComponent<KomaController>();
-		}
-    }
-
-    /// <summary>
-    /// <para>CheckKing</para>
-    /// <para></para>
-    /// </summary>
-    private void CheckKing()
-	{
-		// ƒLƒ“ƒO‚ª‘¶İ‚µ‚È‚¢
-		if (!_komaCtrl.OwnKomas.Contains(_myKing))
-		{
-			OnDisappearKing?.Invoke();
-		}
-	}
 }
